355de64c0e6ac176ef04f9b82910b39f1a6cba98
regchiu <regx1385@gmail.com>
2023-05-03 18:01:55 +0800

    feat: head meta viewport

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 const ClientOnlyTodoList = dynamic(() => import('@/components/todo-list'), {
   ssr: false,
 })
 
 export default function HomePage() {
-  return <ClientOnlyTodoList />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <ClientOnlyTodoList />
+    </>
+  )
 }
