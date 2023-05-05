e0fbc69281da38ee32b1a4654ae8694636b8f1d2
regchiu <regx1385@gmail.com>
2023-05-05 18:26:46 +0800

    refactor: TodoList component

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -18,17 +18,203 @@ type Todo = {
 
 type Visibility = 'all' | 'active' | 'completed'
 type Sorting = 'ascending' | 'descending'
+type EditTodoId = number | null
 
 const STORAGE_KEY = 'next-todo-list'
 
-export default function TodoList() {
+function NewTodoInput({ onAdd }: { onAdd: (newTodo: Todo) => void }) {
   const [todoText, setTodoText] = useState<string>('')
+
+  function handleKeyUpEnter(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      const text = todoText.trim()
+      if (text) {
+        setTodoText('')
+        onAdd({
+          id: Date.now(),
+          text,
+          completed: false,
+        })
+      }
+    }
+  }
+
+  return (
+    <TextInput
+      type="text"
+      autoFocus
+      placeholder="What needs to be done?"
+      value={todoText}
+      shadow={true}
+      rightIcon={MdKeyboardBackspace}
+      onChange={(e) => setTodoText(e.target.value)}
+      onKeyUp={handleKeyUpEnter}
+    />
+  )
+}
+
+function ActiveTodoRemainingText({ remaining }: { remaining: number }) {
+  let remainingUnitText = 'item'
+  if (remaining > 1) {
+    remainingUnitText = 'items'
+  }
+
+  return (
+    <div className="dark:text-gray-300">
+      <span>{remaining}</span> <span>{remainingUnitText}</span>{' '}
+      <span>left</span>
+    </div>
+  )
+}
+
+function ClearCompletedButton({
+  className,
+  todos,
+  onRemove,
+}: {
+  className?: string
+  todos: Array<Todo>
+  onRemove: (newTodos: Array<Todo>) => void
+}) {
+  function handleClick() {
+    onRemove(todos.filter((todo) => !todo.completed))
+  }
+
+  return (
+    <Button
+      className={className}
+      size="sm"
+      outline={true}
+      color="info"
+      onClick={handleClick}
+    >
+      Clear Completed
+    </Button>
+  )
+}
+
+function SortingButton({
+  sorting,
+  onSort,
+}: {
+  sorting: Sorting
+  onSort: (newSorting: Sorting) => void
+}) {
+  function handleClick(sorting: Sorting) {
+    onSort(sorting)
+  }
+
+  if (sorting === 'descending') {
+    return (
+      <Button size="sm" onClick={() => handleClick('ascending')}>
+        <FaSortAmountDown className="h-5 w-5" />
+      </Button>
+    )
+  } else {
+    return (
+      <Button size="sm" onClick={() => handleClick('descending')}>
+        <FaSortAmountUp className="h-5 w-5" />
+      </Button>
+    )
+  }
+}
+
+function TodoItemDescription({
+  todo,
+  editTodoId,
+  onEdit,
+  onSave,
+}: {
+  todo: Todo
+  editTodoId: EditTodoId
+  onEdit: (newTodo: Todo) => void
+  onSave: (newEditTodoId: EditTodoId) => void
+}) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onEdit({ ...todo, text: event.target.value })
+  }
+
+  function handleKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      onSave(null)
+    }
+  }
+
+  let itemTextContent = <>{todo.text}</>
+  if (todo.completed) {
+    itemTextContent = <s>{todo.text}</s>
+  }
+
+  let itemLabelContent = <>{itemTextContent}</>
+
+  if (editTodoId === todo.id) {
+    itemLabelContent = (
+      <TextInput
+        type="text"
+        value={todo.text}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+      />
+    )
+  }
+
+  return <Label className="break-all">{itemLabelContent}</Label>
+}
+
+function TodoItemAction({
+  todo,
+  editTodoId,
+  onEdit,
+  onSave,
+  onDelete,
+}: {
+  todo: Todo
+  editTodoId: EditTodoId
+  onEdit: (newEditTodoId: EditTodoId) => void
+  onSave: (newEditTodoId: EditTodoId) => void
+  onDelete: (newEditTodoId: EditTodoId) => void
+}) {
+  function handleEditClick() {
+    onEdit(todo.id)
+  }
+  function handleSaveClick() {
+    onSave(null)
+  }
+  function handleDeleteClick() {
+    onDelete(todo.id)
+  }
+
+  let editButton = (
+    <Button size="sm" onClick={handleEditClick}>
+      <MdEdit />
+    </Button>
+  )
+
+  if (editTodoId === todo.id) {
+    editButton = (
+      <Button size="sm" color="success" onClick={handleSaveClick}>
+        <MdSave />
+      </Button>
+    )
+  }
+
+  return (
+    <>
+      {editButton}
+      <Button size="sm" color="failure" onClick={handleDeleteClick}>
+        <MdDelete />
+      </Button>
+    </>
+  )
+}
+
+export default function TodoList() {
   const [todos, setTodos] = useState<Array<Todo>>(
     JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
   )
   const [visibility, setVisibility] = useState<Visibility>('all')
   const [searchText, setSearchText] = useState<string>('')
-  const [editTodoId, setEditTodoId] = useState<number | null>(null)
+  const [editTodoId, setEditTodoId] = useState<EditTodoId>(null)
   const [sorting, setSorting] = useState<Sorting>('descending')
 
   const remaining = todos.filter((todo) => !todo.completed).length
@@ -78,23 +264,6 @@ export default function TodoList() {
       : filteredTodos
   }
 
-  function typeEnterToAddTodo(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter') {
-      const text = todoText.trim()
-      if (text) {
-        setTodos([
-          ...todos,
-          {
-            id: Date.now(),
-            text,
-            completed: false,
-          },
-        ])
-        setTodoText('')
-      }
-    }
-  }
-
   function changeTodo(nextTodo: Todo) {
     setTodos(
       todos.map((todo) => {
@@ -107,41 +276,14 @@ export default function TodoList() {
     )
   }
 
-  function deleteTodo(todoId: number) {
-    setTodos(todos.filter((todo) => todo.id !== todoId))
-  }
-
-  function removeCompleted() {
-    setTodos(todos.filter((todo) => !todo.completed))
-  }
-
-  function typeEnterToSaveEdit(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter') {
-      setEditTodoId(null)
-    }
-  }
-
   return (
     <div className="w-full format lg:format-lg">
       <h1 className="text-center dark:text-gray-300">TODO LIST</h1>
-      <TextInput
-        type="text"
-        autoFocus
-        placeholder="What needs to be done?"
-        value={todoText}
-        shadow={true}
-        rightIcon={MdKeyboardBackspace}
-        onChange={(e) => setTodoText(e.target.value)}
-        onKeyUp={typeEnterToAddTodo}
-      />
-      {todos.length > 0 ? (
+      <NewTodoInput onAdd={(newTodo) => setTodos([...todos, newTodo])} />
+      {todos.length > 0 && (
         <div className="w-full not-format mt-4 flex flex-col gap-4">
           <section className="flex flex-col gap-4">
-            <div className="dark:text-gray-300">
-              <span>{remaining}</span>{' '}
-              <span>{remaining === 1 ? 'item' : 'items'}</span>{' '}
-              <span>left</span>
-            </div>
+            <ActiveTodoRemainingText remaining={remaining} />
             <div>
               <TextInput
                 type="text"
@@ -153,15 +295,10 @@ export default function TodoList() {
               />
             </div>
             <div className="flex flex-row w-full gap-4">
-              {sorting === 'descending' ? (
-                <Button size="sm" onClick={() => setSorting('ascending')}>
-                  <FaSortAmountDown className="h-5 w-5" />
-                </Button>
-              ) : (
-                <Button size="sm" onClick={() => setSorting('descending')}>
-                  <FaSortAmountUp className="h-5 w-5" />
-                </Button>
-              )}
+              <SortingButton
+                sorting={sorting}
+                onSort={(newSorting) => setSorting(newSorting)}
+              />
               <Button.Group>
                 <Button
                   size="sm"
@@ -185,15 +322,11 @@ export default function TodoList() {
                   Completed
                 </Button>
               </Button.Group>
-              <Button
-                size="sm"
-                outline={true}
-                color="info"
+              <ClearCompletedButton
                 className={cn({ hidden: remaining >= todos.length })}
-                onClick={() => removeCompleted()}
-              >
-                Clear Completed
-              </Button>
+                todos={todos}
+                onRemove={(newTodos) => setTodos(newTodos)}
+              />
             </div>
           </section>
           <ul
@@ -234,52 +367,32 @@ export default function TodoList() {
                     />
                   </div>
                   <div className="flex-auto">
-                    <Label
-                      className="break-all"
-                      onDoubleClick={() => setEditTodoId(todo.id)}
-                    >
-                      {editTodoId === todo.id ? (
-                        <TextInput
-                          type="text"
-                          value={todo.text}
-                          onChange={(e) =>
-                            changeTodo({ ...todo, text: e.target.value })
-                          }
-                          onKeyUp={typeEnterToSaveEdit}
-                        />
-                      ) : (
-                        <>{todo.completed ? <s>{todo.text}</s> : todo.text}</>
-                      )}
-                    </Label>
+                    <TodoItemDescription
+                      todo={todo}
+                      editTodoId={editTodoId}
+                      onEdit={(newTodo) => changeTodo(newTodo)}
+                      onSave={(newEditTodoId) => setEditTodoId(newEditTodoId)}
+                    />
                   </div>
                   <div className="flex flex-row gap-2">
-                    {editTodoId === todo.id ? (
-                      <Button
-                        size="sm"
-                        color="success"
-                        onClick={() => setEditTodoId(null)}
-                      >
-                        <MdSave />
-                      </Button>
-                    ) : (
-                      <Button size="sm" onClick={() => setEditTodoId(todo.id)}>
-                        <MdEdit />
-                      </Button>
-                    )}
-                    <Button
-                      size="sm"
-                      color="failure"
-                      onClick={() => deleteTodo(todo.id)}
-                    >
-                      <MdDelete />
-                    </Button>
+                    <TodoItemAction
+                      todo={todo}
+                      editTodoId={editTodoId}
+                      onEdit={(newEditTodoId) => setEditTodoId(newEditTodoId)}
+                      onSave={(newEditTodoId) => setEditTodoId(newEditTodoId)}
+                      onDelete={(newEditTodoId) =>
+                        setTodos(
+                          todos.filter((todo) => todo.id !== newEditTodoId)
+                        )
+                      }
+                    />
                   </div>
                 </div>
               </li>
             ))}
           </ul>
         </div>
-      ) : null}
+      )}
     </div>
   )
 }
